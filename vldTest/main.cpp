73ba4f617346d7d726d828a0d6b4c438f2811fef
YaoXing <YaoXing@YAOXING-PC>
2017-09-19 10:51:11 +0800

    添加项目文件。

---
+++ vldTest/main.cpp
@@ -0,0 +1,20 @@
+#include"vld.h"
+#include"iostream"
+
+using namespace std;
+
+void fun()
+{
+	char* tmp = new char[10];
+	memset(tmp, 0, 10);
+	delete tmp;
+}
+
+int main(int argc, char* argv[])
+{
+	fun();
+
+	cout << "ok!" << endl;
+
+	return 0;
+}
